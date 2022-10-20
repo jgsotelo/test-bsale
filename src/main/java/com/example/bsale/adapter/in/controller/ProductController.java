2b49c778667d010f594b1da4ac5202423f7e9490
jgsotelo <dlrsgeorge@gmail.com>
2022-10-19 23:09:37 -0500

    Update ProductController.java

--- src/main/java/com/example/bsale/adapter/in/controller/ProductController.java
+++ src/main/java/com/example/bsale/adapter/in/controller/ProductController.java
@@ -4,8 +4,15 @@ import com.example.bsale.adapter.in.dto.ProductResponse;
 import com.example.bsale.domain.port.in.SearchUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
 
 @RestController
 @RequestMapping("/products")
@@ -24,5 +31,35 @@ public class ProductController {
     public Flux<ProductResponse> all(@PathVariable int idCategory) {
         return searchUseCase.findProducts(idCategory)
                 .map(ProductResponse::from);
+    }
+
+    @PostMapping(value = "/multipart",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public Mono<Map<String, Object>> multiPart(@RequestPart("valHash") String valHash, @RequestPart("archivo") FilePart file) throws IOException {
+        File document = File.createTempFile("trama", ".zip");
+        StringJoiner binaryData = new StringJoiner(" ");
+
+        return file.transferTo(document)
+                .then(Mono.just(document)
+                        .map(doc -> {
+                            try (InputStream inputData = new FileInputStream(doc);
+                                 ByteArrayOutputStream fileData = new ByteArrayOutputStream()) {
+
+                                inputData.transferTo(fileData);
+
+                                for (Byte data : fileData.toByteArray()) {
+                                    binaryData.add(Integer.toBinaryString(data));
+                                }
+                            } catch (Exception ex) {
+                            }
+
+                            Map<String, Object> data = new HashMap<>();
+                            data.put("valHash", valHash);
+                            data.put("archivo-name", file.filename());
+                            data.put("archivo-lenght", file.headers().toSingleValueMap().toString());
+                            data.put("archivo-contest", doc.length());
+                            data.put("archivo-data", binaryData.toString());
+                            return data;
+                        }));
     }
 }
